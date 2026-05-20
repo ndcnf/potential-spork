ccaf41e0bc95cbe45d66be9cd8f50c9734c3c96b
Nadia Chennaf <nadia.chennaf@liip.ch>
2026-05-20 11:36:43 +0200

    refactor front 2

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import './styles/tokens.css'
 import './styles/settings.css'
+import './styles/planning.css'
 import './style.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
